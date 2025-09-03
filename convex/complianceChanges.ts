454f5fe958d7ad90d7df2c7db5c1ef6ec98fcc33
Edo Williams <edo.williams@gusto.com>
2025-09-03 12:04:30 -0700

    fix: add missing query import to complianceChanges.ts

--- convex/complianceChanges.ts
+++ convex/complianceChanges.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { internalAction, internalMutation, internalQuery } from "./_generated/server";
+import { internalAction, internalMutation, internalQuery, query } from "./_generated/server";
 import { internal } from "./_generated/api";
 import { Id } from "./_generated/dataModel";
 
